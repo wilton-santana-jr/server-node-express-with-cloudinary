ce10cbb5e865f80bd48670f916f9460fa40e7cea
wilton-santana-jr <wsantana3@gmail.com>
2023-09-08 15:48:10 -0300

    commit servidor node express robusto com upload de imagens no cloudinary

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const cloudinary = require('cloudinary').v2
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//cloudinary.config({ 
+//  cloud_name: 'driib5ksx', 
+//  api_key: '995935368578545', 
+//  api_secret: 'x2sJ-G1mKChNxJwCHb1wD4UXObw' 
+//});
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.api_key, 
+  api_secret: process.env.api_secret 
+});
+
+  
+//app.use((request, response) => {
+//   response.json({ message: 'Olá! Este é seu servidor express que atenderá suas requisições!' }); 
+//});
+
+// endpoint da api para mostrar uma mensagem que o servidor está atendendo requisições
+app.get("/", (request, response) => {
+    response.json({ message: "Olá! Este é seu servidor express que atenderá suas requisições!" });
+});
+
+
+// endpoint da api para upload de uma imagem no cloudinary
+app.post("/upload-image", (request, response) => {
+	
+	// collected image from a user
+    const data = {
+      image: request.body.image,
+    }
+
+    // upload da imagem aqui
+    cloudinary.uploader.upload(data.image)
+    .then((result) => {
+      response.status(200).send({
+        message: "success",
+        result,
+      });
+    }).catch((error) => {
+      response.status(500).send({
+        message: "failure",
+        error,
+      });
+    });
+	
+	
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,65 @@
+//const http = require('http');
+//const app = require('./app');
+
+//server com o express configurado no app.js
+//app.set('port', 3000);
+//const server = http.createServer(app);
+
+//server sem o express
+//const server = http.createServer(
+//  (request, response) => {
+//    response.end('Olá! Esta é a resposta do seu servidor!');
+//  }
+//);
+
+//server.listen(3000);
+   
+   
+//teste o servidor com o comando nodemon index
+const http = require('http');
+const app = require('./app');
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
